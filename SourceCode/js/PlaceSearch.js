6ff73c357d813f9b6ca322cf29098b1ee89d530c
neerajpadarthi <42651065+neerajpadarthi@users.noreply.github.com>
2018-10-24 14:17:20 -0500

    Home page sorting the output

--- SourceCode/js/PlaceSearch.js
+++ SourceCode/js/PlaceSearch.js
@@ -2,10 +2,12 @@ angular.module('indexpage',[])
     .controller('indexctrl', function($scope, $http) {
         $scope.getSearchResult = function() {
             $scope.placesArray =[];
-            $scope.picsArray=[];
             $scope.reviews=[];
+            $scope.placeRatings=[];
             $scope.weekdayHours=[];
-            var placeId="";
+            $scope.placeids=[];
+
+            //Gettin the interest field value if the user doesn't select anything no interest is passed to the api request.
             var interestValue=(document.getElementById("interest").value).toLowerCase();
             if(interestValue=="select")
             {
@@ -15,110 +17,133 @@ angular.module('indexpage',[])
             {
                 interestValue ="&type="+(document.getElementById("interest").value).toLowerCase();
             }
+
+            //Here the code is written to get the places of particular destination with.without interest.
+            //From the output of url request we take the placeid,name,address and rating
+            //we will do the places sort on basis of rating
             $http.get("https://maps.googleapis.com/maps/api/place/textsearch/json?query="+$scope.searchDestination+"+point+of+interest"+interestValue+"&language=en&key=AIzaSyAk8FdCcWPekxegcpFkUAL5frrMc73F-4E").then(function(data)
             {
                 $scope.listheader="Here are the places of the searched destination and priority";
                 try {
-                    var results=data.data.results;
+                    var results=data.data.results.sort((a, b) => a.rating - b.rating);
 
                     var length=data.data.results.length;
 
                     for(var j=0;j<length;j++)
                     {
-                        var photoReference= results[j].photos[0].photo_reference;
-                        placeId=results[j].place_id;
-
-                        $scope.addressHeader="Address :- ";
-                        $scope.nameHeader="Place Name :- ";
-                        //$scope.place_id=results[j].place_id;
-                        $scope.ratingHeader="Rating :-";
+                        var photoReference = results[j].photos[0].photo_reference;
 
 
-                        var image="https://maps.googleapis.com/maps/api/place/photo?maxwidth=100&photoreference="+photoReference+"&key=AIzaSyAk8FdCcWPekxegcpFkUAL5frrMc73F-4E";
-                        $scope.picsArray.push(image);
+                        $scope.addressHeader = "Address :- ";
+                        $scope.nameHeader = "Place Name :- ";
+                        $scope.ratingHeader = "Rating :-";
+                        $scope.description = "Description :-";
 
+                        $scope.placeids.push(results[j].rating + "###" +results[j].place_id);
 
+                        var image = "https://maps.googleapis.com/maps/api/place/photo?maxwidth=100&photoreference=" + photoReference + "&key=AIzaSyAk8FdCcWPekxegcpFkUAL5frrMc73F-4E";
+                        $scope.placeRatings.push(results[j].rating);
 
-                        var appendedstring= results[j].rating+"###"+results[j].formatted_address+"***"+results[j].name;
+                        var appendedstring = results[j].rating + "###" + results[j].formatted_address + "***" + results[j].name + "^^^" + image;
                         $scope.placesArray.push(appendedstring);
-
-                        $http.get("https://maps.googleapis.com/maps/api/place/details/json?placeid="+placeId+"&key=AIzaSyAk8FdCcWPekxegcpFkUAL5frrMc73F-4E").then(function(placedata)
-                        {
-                            $scope.weekdayHours_week=[];
-                            var documents=[];
-                            if(placedata.data.result.reviews!= undefined)
-                            {
-                                for(var i=0;i<1;i++) {
-
-                                    $scope.reviewHeader = "Reviews";
-
-                                    $scope.author_name_header = "Author Name :- ";
-                                    $scope.reviewtime_header = "Time of review :- ";
-                                    $scope.comment_header = "Comment :-";
-                                    $scope.user_rating_header = "User Rating :- ";
-
-
-                                    $scope.author_Name = placedata.data.result.reviews[i].author_name;
-                                    $scope.reviewtime = placedata.data.result.reviews[i].relative_time_description;
-                                    $scope.comment = placedata.data.result.reviews[i].text;
-                                    $scope.user_rating = placedata.data.result.reviews[i].rating;
-
-
-                                    documents.push($scope.author_Name + '@@' + $scope.reviewtime + '##' + $scope.comment + '$$'+$scope.user_rating);
-
-                                }
-                                for(var x=0;x<documents.length;x++) {
-                                    $scope.reviews.push(documents[x]);
-                                }
-
-
-                                    $scope.weeklyhoursheader = "Weeekly Hours";
-                                    for (var k = 0; k < 7; k++) {
-                                            try {
-                                                var weekday_timings = placedata.data.result.opening_hours.weekday_text[k];
-                                                $scope.weekdayHours_week.push(weekday_timings);
-                                            }
-                                            catch (e) {
-                                                switch (k) {
-                                                    case 0:
-                                                        $scope.weekdayHours_week.push("Monday:Closed");
-                                                        break;
-                                                    case 1:
-                                                        $scope.weekdayHours_week.push("Tuesday:Closed");
-                                                        break;
-                                                    case 2:
-                                                        $scope.weekdayHours_week.push("Wednesday:Closed");
-                                                        break;
-                                                    case 3:
-                                                        $scope.weekdayHours_week.push("Thursday:Closed");
-                                                        break;
-                                                    case 4:
-                                                        $scope.weekdayHours_week.push("Friday:Closed");
-                                                        break;
-                                                    case 5:
-                                                        $scope.weekdayHours_week.push("Saturday:Closed");
-                                                        break;
-                                                    case 6:
-                                                        $scope.weekdayHours_week.push("Sunday:Closed");
-                                                        break;
-                                                }
-                                            }
-                                    }
-                                    $scope.weekdayHours.push($scope.weekdayHours_week);
-                            }
-                            else
-                                {
-
-                                }
-                        })
-
                     }
-                    $scope.placesArray.sort();
-                    console.log($scope.placesArray );
+
+                    //$scope.placesArray.sort();
                 }
                 catch(err){
                 }
             })
 
+
+
+            //Here we are requesting for a particular place details with the help of place id's
+            //First we are going to sort the place ids
+            //since requests can take time in above function so kept a waiting time to this logic
+            //After getting the output we will get the the following fields i.e reviews and weekly hours
+            setTimeout(function ()
+            {
+                try{
+                    $scope.placeids.sort((a, b) => a.rating - b.rating);
+                    for( var z=0;z<$scope.placeids.length;z++)
+                    {
+                        var placeId=$scope.placeids[z].substring($scope.placeids[z].indexOf("###")+3,$scope.placeids[z].length);
+                        console.log(placeId);
+
+                        $http.get("https://maps.googleapis.com/maps/api/place/details/json?placeid="+placeId+"&key=AIzaSyAk8FdCcWPekxegcpFkUAL5frrMc73F-4E").then(function(placedata)
+                        {
+                            $scope.weekdayHours_week=[];
+                            var documents=[];
+
+                            //     for(var i=0;i<1;i++) {
+
+                            $scope.reviewHeader = "Reviews";
+
+                            $scope.author_name_header = "Author Name :- ";
+                            $scope.reviewtime_header = "Time of review :- ";
+                            $scope.comment_header = "Comment :-";
+                            $scope.user_rating_header = "User Rating :- ";
+
+
+                            $scope.author_Name = placedata.data.result.reviews[0].author_name;
+                            $scope.reviewtime = placedata.data.result.reviews[0].relative_time_description;
+                            $scope.comment = placedata.data.result.reviews[0].text;
+                            $scope.user_rating = placedata.data.result.reviews[0].rating;
+
+
+                            documents.push($scope.author_Name + '@@' + $scope.reviewtime + '##' + $scope.comment + '$$'+$scope.user_rating);
+
+                            // }
+                            $scope.reviews.push($scope.author_Name + '@@' + $scope.reviewtime + '##' + $scope.comment + '$$'+$scope.user_rating);
+                            console.log($scope.author_Name + '@@' + $scope.reviewtime + '##' + $scope.comment + '$$'+$scope.user_rating);
+
+
+                            // for(var x=0;x<documents.length;x++) {
+                            //     $scope.reviews.push((documents[x]).toString());
+                            // }
+
+
+                            $scope.weeklyhoursheader = "Weeekly Hours";
+                            for (var k = 0; k < 7; k++) {
+                                try {
+                                    var weekday_timings = placedata.data.result.opening_hours.weekday_text[k];
+                                    $scope.weekdayHours_week.push(weekday_timings);
+                                }
+                                catch (e) {
+                                    // switch (k) {
+                                    $scope.weekdayHours_week.push("All Working Days");
+                                    break;
+                                    //     case 0:
+                                    //         $scope.weekdayHours_week.push("Monday:Closed");
+                                    //         break;
+                                    //     case 1:
+                                    //         $scope.weekdayHours_week.push("Tuesday:Closed");
+                                    //         break;
+                                    //     case 2:
+                                    //         $scope.weekdayHours_week.push("Wednesday:Closed");
+                                    //         break;
+                                    //     case 3:
+                                    //         $scope.weekdayHours_week.push("Thursday:Closed");
+                                    //         break;
+                                    //     case 4:
+                                    //         $scope.weekdayHours_week.push("Friday:Closed");
+                                    //         break;
+                                    //     case 5:
+                                    //         $scope.weekdayHours_week.push("Saturday:Closed");
+                                    //         break;
+                                    //     case 6:
+                                    //         $scope.weekdayHours_week.push("Sunday:Closed");
+                                    //         break;
+                                    // }
+                                }
+                            }
+                            $scope.weekdayHours.push($scope.weekdayHours_week);
+                        })
+
+                    }
+                }
+                catch (e) {
+
+                }
+            }, 1000);
         }
     });
